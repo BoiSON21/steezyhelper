04ba43643ff93bd800470d1cf5d928e8e0baa1fd
Fabian Binder <fabian@bizfactory.tech>
2023-06-15 14:51:17 +0200

    updated nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@element-plus/nuxt'],
   css: ['~/assets/global.css'],
+  app: {
+    baseUrl: '/steezyhelper/',
+  },
   router: {
     base: '/steezyhelper/'
   },
