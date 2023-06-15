ce5467a678bfb4f7cde967df0b7376e3c68d07c1
Fabian Binder <fabian@bizfactory.tech>
2023-06-15 14:02:32 +0200

    reinstalled modues

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  target: 'static',
   devtools: { enabled: true },
   modules: ['@element-plus/nuxt'],
   css: ['~/assets/global.css'],
-  target: 'static',
   router: {
     base: '/steezyhelper/'
   }
