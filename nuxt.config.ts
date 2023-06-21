bd1c43a20423dbb1f5b97eb491dfaf2f703d1f7b
fabian@bizfactory.tech <fabian@bizfactory.tech>
2023-06-22 01:00:42 +0200

    changed configs to support vercel deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import dotenv from 'dotenv'
+
+const isGitHubPages = process.env.DEPLOY_ENV === 'GH_PAGES'
+const baseUrl = process.env.BASE_URL || '/'
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@element-plus/nuxt'],
   css: ['~/assets/global.css'],
   app: {
-    baseURL: '/steezyhelper/',
+    baseURL: isGitHubPages? baseUrl : '/',
     head: {
       link: [{ rel: 'icon', type: 'image/png', href: '/steezyhelper/favicon.ico' }]
     }
